a68281588da538664cce68f4a8d3796228186bbe
Mawrwane Tahiri <marwanetahiripro@gmail.com>
2026-05-06 13:52:50 +0100

    Add target pay calculator feature

--- Overtime_Tracker.py
+++ Overtime_Tracker.py
@@ -19,12 +19,31 @@ class OvertimeTracker:
 
     def viewall(self):
         print(f"you have worked {self.hours} hours.")
+    
+    def targetpay(self):
+        pay = int(input("How much do you get paid per hour?: "))
+
+        targetpay = int(input("How much would you like to get paid this month?: "))
+
+        hoursleft = round((targetpay - (self.hours * pay)) / pay, 2)
+
+        if hoursleft > 0:
+            print(f"you need to work {hoursleft} hours to attain that amount") 
+        else:
+            print(f"you have reached £ {targetpay} or more this month")
+
+
+
+
         
 
 
 ot = OvertimeTracker()
 
-ot.addnew(2)
+ot.addnew(1)
 
 ot.viewall()
+
+
+print(ot.targetpay())
 
