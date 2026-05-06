dd2bbec5c8987bf18ca21f1930355c3ef5294305
Mawrwane Tahiri <marwanetahiripro@gmail.com>
2026-05-06 15:44:17 +0100

    Refactor to store individual sessions with dates

--- Overtime_Tracker.py
+++ Overtime_Tracker.py
@@ -1,36 +1,57 @@
 import json
 from pathlib import Path
 
+
 class OvertimeTracker:
     def __init__(self):
+        self.overtimelist = []
+        self.overtimedict = {}
+
+
         hoursfile = Path("overtimehours.json")
 
         if hoursfile.exists():
             with open('overtimehours.json', 'r') as file:
-                self.hours = json.load(file)
+                self.overtimelist = json.load(file)
         else:
-            self.hours = 0
+            self.overtimelist = []
 
-    def addnew(self, value):
-        self.hours += value
+            
+
+    def addnew(self):
+        overtimedate = input("When was this? DD-MM-YY: ")
+        overtimehours = int(input("How many hours did you do?: "))
+
+        self.overtimelist.append({overtimedate: overtimehours})
 
         with open('overtimehours.json', 'w') as file:
-            json.dump(self.hours, file)
+            json.dump(self.overtimelist, file)
+
+
 
     def viewall(self):
-        print(f"you have worked {self.hours} hours.")
+        print(f"you have worked {self.totalhours()} hours.")
     
     def targetpay(self):
         pay = int(input("How much do you get paid per hour?: "))
 
         targetpay = int(input("How much would you like to get paid this month?: "))
 
-        hoursleft = round((targetpay - (self.hours * pay)) / pay, 2)
+        hoursleft = round((targetpay - (self.totalhours() * pay)) / pay, 2)
 
         if hoursleft > 0:
             print(f"you need to work {hoursleft} more hours to attain that amount") 
         else:
             print(f"you have reached £ {targetpay} or more this month")
+
+    def totalhours(self):
+        self.total = 0
+        
+        for dicts in self.overtimelist:
+            for j in dicts.values():
+                self.total = self.total + j 
+        return self.total
+
 
 ot = OvertimeTracker()
 
@@ -38,8 +59,7 @@ while True:
     choice = input("1. Log overtime\n 2. View total hours\n 3. Target pay calculator\n 4. Exit\n Enter Number: ")
 
     if choice == "1":
-        lognew = int(input("How many hours would you log?: "))
-        ot.addnew(lognew)
+        ot.addnew()
     
     elif choice == "2":
         ot.viewall()
